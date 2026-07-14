f807dabc215428714d7a401fce30ee52c285db46
Kejlo523 <kejlo234@gmail.com>
2026-07-14 18:54:15 +0200

    Fixed grid in schedule, and added hidden achievements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "pl.kejlo.zutnik"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
