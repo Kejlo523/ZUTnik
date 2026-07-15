84f3e4dd972ccfec092e251e14fe23babbd32b3b
Kejlo523 <kejlo234@gmail.com>
2026-07-15 21:59:13 +0200

    fixed tiles not showing in 2x1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "pl.kejlo.zutnik"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.15"
+        versionCode = 16
+        versionName = "1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
